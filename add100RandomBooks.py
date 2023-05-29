52f27f7380709211d9c08609b84a0ec932ee35f8
Exchangers12 <ilviragaanova@gmail.com>
2023-05-29 01:14:05 +0000

    change to delete first and last 5 books

--- add100RandomBooks.py
+++ add100RandomBooks.py
@@ -20,17 +20,16 @@ def getAuthToken():
     else:
         raise Exception(f"Status code {r.status_code} and text {r.text}, while trying to Auth.")
 
-def addBook(book, apiKey):
-    r = requests.post(
-        f"{APIHOST}/api/v1/books", 
+def deleteBOok(id):
+    r = requests.get(
+        f"{APIHOST}/api/v1/books/{id}", 
         headers = {
             "Content-type": "application/json",
             "X-API-Key": apiKey
             },
-        data = json.dumps(book)
     )
     if r.status_code == 200:
-        print(f"Book {book} added.")
+        print(f"Book ID: {id} was deleted!")
     else:
         raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to add book {book}.")
 
@@ -38,11 +37,14 @@ def addBook(book, apiKey):
 apiKey = getAuthToken()
 
 # Using the faker module, generate random "fake" books
-fake = Faker()
-for i in range(1, 126):
-    fakeTitle = fake.catch_phrase()
-    fakeAuthor = fake.name()
-    fakeISBN = fake.isbn13()
-    book = {"id":i, "title": fakeTitle, "author": fakeAuthor, "isbn": fakeISBN}
+# fake = Faker()
+for i in range(1, 6):
+    # fakeTitle = fake.catch_phrase()
+    # fakeAuthor = fake.name()
+    # fakeISBN = fake.isbn13()
+    # book = {"id":i, "title": fakeTitle, "author": fakeAuthor, "isbn": fakeISBN}
     # add the new random "fake" book using the API
-    addBook(book, apiKey) 
+    deleteBOok(i)
+
+for i in range(120, 125):
+    deleteBOok(i)
